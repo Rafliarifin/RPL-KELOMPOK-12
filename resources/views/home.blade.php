ac377e74a7f14d43e7f5032f538ea8adf8ef1d38
IqraJr <iqrafauzanakbar@gmail.com>
2024-10-19 21:43:13 +0800

    update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,20 +1,41 @@
-{{-- resources/views/home.blade.php --}}
-
-@extends('layout') {{-- Pastikan ini mengarah ke layout yang benar --}}
+@extends('welcome')
 
 @section('content')
 
 
-    <section class="bg-cover bg-center py-24" style="background-image: url('{{ asset('images/hero-bg.jpg') }}')">
-        <div class="container mx-auto text-center text-white">
-            <h1 class="text-5xl font-bold mb-4 text-white-500">Jasa Rental Mobil Online</h1><br><br>
-            <a href="{{ route('cars.index') }}" class="btn btn-primary text-white-500 text-4xl bg-blue">Pesan Sekarang</a>
+    <!-- Bagian konten utama dengan warna background -->
+    <section class="bg-primary py-24">
+        <div class="container mx-auto text-center">
+            <h1 class="text-3xl font-bold  mb-4" style="color: #fdfac7 ;font-family: 'poppins'";>Jasa Rental Mobil Online</h1><br><br>
+            <a href="{{ route('cars.index') }}" class="btn btn-secondary text-lg font-bold py-5 px-9 rounded-lg">Pesan Sekarang</a><br><br><br><br><br><br><br><br><br><br><br>
+            <p class="text-2xl font-semibold text-dark" style="font-family: 'Comic Sans MS', cursive, sans-serif;">"Nikmati Kemudahan Menyewa Mobil Dengan Layanan Kami"</p><br><br>
+
         </div>
+        
+        <div class="container mx-auto text-center">
+        <div class="flex justify-around">
+            <div class="feature text-white">
+                <i class="fas fa-shield-alt fa-2x text-dark mb-1"></i>
+                <h3 class="text-xl font-semibold ">Asuransi Mobil</h3>
+                <p class="text-lg ">Mobil Anda aman dengan asuransi lengkap.</p>
+            </div>
+            <div class="feature text-white">
+                <i class="fas fa-car-side fa-2x text-dark mb-2"></i>
+                <h3 class="text-xl font-semibold">Pengantaran Gratis</h3>
+                <p class="text-lg">Kami antar mobil ke lokasi Anda.</p>
+            </div>
+            <div class="feature text-white">
+                <i class="fas fa-clock fa-2x text-dark mb-2"></i>
+                <h3 class="font-semibold">Layanan 24 Jam</h3>
+                <p class="text-lg">Layanan siap membantu kapan saja.</p>
+            </div>
+            </div>
+        </div>
+
     </section>
 
     
 
-    <footer>
-        <!-- Footer content here -->
-    </footer>
+    
+        
 @endsection
